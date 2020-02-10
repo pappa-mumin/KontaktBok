924ce5490f2314f25ae0ad6f5ea3572a5e759abb
beehive <oscar.gerdin@gmail.com>
2020-02-10 15:29:16 +0100

    Added KontaktBokTest

---
+++ src/test/KontaktBokTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.KontaktBok;
+import main.Person;
+
+class KontaktBokTest {
+	
+	private KontaktBok konBok;
+	private Person testPerson;
+	ArrayList<Person> KontaktLista = new ArrayList<Person>();
+
+	@BeforeEach
+	void nyKontaktBok() {
+		konBok = new KontaktBok();
+	}
+
+	@BeforeEach
+	void PersonerIArrayen() {
+		testPerson = new Person("Förnamn","Efternamn", "Telefonnummer", "Melj");
+		KontaktLista.add(testPerson);
+	}
+	
+	@Test
+	void visaTest() {
+		
+		KontaktLista.visa();
+	}
+
+}
