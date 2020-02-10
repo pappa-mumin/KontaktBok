8d1a24066090661bd6fbf56355e75a26ea39f877
beehive <oscar.gerdin@gmail.com>
2020-02-10 14:19:03 +0100

    Added kontaktbok 4real

--- src/main/KontaktBok.java
+++ src/main/KontaktBok.java
@@ -1,5 +1,17 @@
 package main;
 
-public class KontaktBok {
+import java.util.ArrayList;
 
+public class KontaktBok {
+		
+		ArrayList<Person> KontaktLista = new ArrayList<Person>();
+		
+	
+		public Person visa(int i) {
+			return KontaktLista.get(i);
+		}
+		
+		public Person taBort(int i) {
+			return KontaktLista.remove(i);
+		}
 }
