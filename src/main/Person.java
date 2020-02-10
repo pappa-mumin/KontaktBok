b29719e5b407fdefdf8d5c77f04905eb15757c78
Louise Ahlqvist <louise.k.ahlqvist@gmail.com>
2020-02-10 14:13:32 +0100

    Skapat klassen person

---
+++ src/main/Person.java
@@ -0,0 +1,48 @@
+package main;
+
+public class Person {
+	
+	private String fnamn, enamn, tel, mejl;
+
+	public Person(String fnamn, String enamn, String tel, String mejl) {
+		super();
+		this.fnamn = fnamn;
+		this.enamn = enamn;
+		this.tel = tel;
+		this.mejl = mejl;
+	}
+
+	public String getFnamn() {
+		return fnamn;
+	}
+
+	public void setFnamn(String fnamn) {
+		this.fnamn = fnamn;
+	}
+
+	public String getEnamn() {
+		return enamn;
+	}
+
+	public void setEnamn(String enamn) {
+		this.enamn = enamn;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getMejl() {
+		return mejl;
+	}
+
+	public void setMejl(String mejl) {
+		this.mejl = mejl;
+	}
+	
+	
+}
