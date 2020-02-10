b042cd99e306550227e04f25177a99ffb145f6d9
Louise Ahlqvist <louise.k.ahlqvist@gmail.com>
2020-02-10 14:39:32 +0100

    Skapat metoden finnsFilen()

--- src/main/Fil.java
+++ src/main/Fil.java
@@ -1,5 +1,21 @@
 package main;
 
+import java.io.File;
+
 public class Fil {
+	
+	private File fil = new File("Kontaktbok/src/main/Kontaktlista.txt");
+	
+	
+	/**
+	 * finnsFilen() testar om filen Kontaktlista.txt finns eller inte
+	 * @return true om filen redan är skapad, false om filen inte finns
+	 * @author Louise
+	 */
+	boolean finnsFilen() {
+		File x = new File("Kontaktbok/src/main/Kontaktlista.txt");
+		boolean finns = x.exists();
+		return finns;
+	}
 
 }
