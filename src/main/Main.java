0c8ce079a94ce87e581dae31b8f2a705be4aa830
Linn <nyren.linn@gmail.com>
2020-02-11 22:28:25 +0100

    Gjort om och skapat ett nytt Ui

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,11 @@
 package main;
 
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Ui ui = new Ui();
 		ui.skapaUI();
-		ui.knappar();
-		
 
 	}
 
--- src/main/Ui.java
+++ src/main/Ui.java
@@ -1,11 +1,11 @@
 package main;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Container;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -15,75 +15,54 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-public class Ui {
-
-	private JFrame forstaFrame;
-	private JLabel headerLabel;
-	private JLabel statusLabel;
-	private JPanel controlPanel;
-	KontaktBok kBok = new KontaktBok();
-	private ArrayList<Person> KontaktLista;
+public class Ui implements ItemListener{
 	
+    final static String LISTAPANEL = "Visa Lista";
+    final static String SOKPANEL = "Sök i lista";
+    final static String LAGGTILLPANEL = "Lägg till kontakt";
+    final static String TABORTPANEL = "Ta bort kontakt";
+    public static JPanel cards;
+    public JScrollPane scrollPane;
+    public JPanel card, card1, card2, card3, card4, sokPanel, laggTillPanel, taBortPanel;
     public String fn, en, tel, mejl;
 	
-	public void skapaUI() {
-		
-		forstaFrame = new JFrame("Kontaktbok");
-		forstaFrame.setSize(800, 600);
-		forstaFrame.setLayout(new GridLayout(5, 1));
-		
-		headerLabel = new JLabel("Hej Head", JLabel.CENTER);
-		headerLabel.setSize(50,50);
-//		statusLabel = new JLabel("Hek status", JLabel.CENTER);
-//		statusLabel.setSize(10, 10);
-		
-		forstaFrame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent windowEvent) {
-				System.exit(0);
-			}
-		});
-		
-		controlPanel = new JPanel();
-		controlPanel.setSize(800, 300);
-//		controlPanel.setLayout(new FlowLayout());
-		
-		forstaFrame.add(headerLabel);
-		forstaFrame.add(controlPanel);
-//		forstaFrame.add(statusLabel);
+    public void addComponentToPane(Container pane) {
+        JPanel comboBoxPane = new JPanel(); 
+        String comboBoxItems[] = { LISTAPANEL, SOKPANEL, LAGGTILLPANEL, TABORTPANEL};
+        JComboBox cb = new JComboBox(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+        comboBoxPane.add(cb);
 
-		forstaFrame.setVisible(true);
+        //Create the "cards".
+        JPanel card1 = new JPanel();
+        card1.add(visaLista());
+         
+        JPanel card2 = new JPanel();
+        card2.add(sokning());
+        
+        JPanel card3 = new JPanel();
+        card3.add(laggTill());
+        
+        JPanel card4 = new JPanel();
+        card4.add(taBort());
+         
+        //Create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, LISTAPANEL);
+        cards.add(card2, SOKPANEL);
+        cards.add(card3, LAGGTILLPANEL);
+        cards.add(card4, TABORTPANEL);
+        
+        pane.add(comboBoxPane, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
 	}
-	
-	public void knappar() {
-		headerLabel.setText("Vad vill du göra i Kontaktboken?");
-		JButton listaKnapp = new JButton("Visa lista");
-		JButton sokKnapp = new JButton("Sök");
-		JButton laggTillKnapp = new JButton("Lägg till");
-		JButton taBortKnapp = new JButton("Ta bort");
-		
-		listaKnapp.setActionCommand("Lista");
-		sokKnapp.setActionCommand("Sök");
-		laggTillKnapp.setActionCommand("Lägg till");
-		taBortKnapp.setActionCommand("Ta bort");
-		
-		listaKnapp.addActionListener(new Knapptryck());
-		sokKnapp.addActionListener(new Knapptryck());
-		laggTillKnapp.addActionListener(new Knapptryck());
-		taBortKnapp.addActionListener(new Knapptryck());
-		
-		controlPanel.add(listaKnapp);
-		controlPanel.add(sokKnapp);
-		controlPanel.add(laggTillKnapp);
-		controlPanel.add(taBortKnapp);
-		
-		forstaFrame.setVisible(true);
-	}
-	
-	public void visaLista() {
-		
+    
+    public JScrollPane visaLista() {
 		ArrayList<String> cars = new ArrayList<String>();
 	    cars.add("Volvo                fndjkshfdsiua");
 	    cars.add("BMW");
@@ -103,36 +82,24 @@ public class Ui {
 	    cars.add("Mazda");
 		JList<String> list = new JList(cars.toArray());
 		list.setLayoutOrientation(JList.VERTICAL);
-		JScrollPane scrollPane = new JScrollPane(list);
-		controlPanel.add(scrollPane);      
-		
-	}
-	
-	public void sokning() {
-//	    JComboBox<String> comboBox = new JComboBox<>();
-//	    comboBox.addItem("Förnamn");
-//	    comboBox.addItem("Efternamn");
-//	    comboBox.addItem("Telefonnummer");
-//	    comboBox.addItem("Mejladress");
-//	    controlPanel.add(comboBox);
-		
-
-
-		JButton sokKnapp = new JButton("Sök");
-		
-	    JPanel panel = new JPanel();
-	    
-		GridLayout gl = new GridLayout(8,1); // 4 rows, 2 columns
-		panel.setLayout(gl);
+		scrollPane = new JScrollPane(list);
+    	return scrollPane;
+    }
+    
+    public JPanel sokning() {
+    	sokPanel = new JPanel();
+    	GridLayout gl = new GridLayout(5,1); 
+    	sokPanel.setLayout(gl);
+    	JButton sokKnapp = new JButton("Sök");
 	    
 		JLabel fornamnLabel = new JLabel("Förnamn: ");
-		JTextField fornamnInput = new JTextField(20);
+		JTextField fornamnInput = new JTextField(30);
 		JLabel efternamnLabel = new JLabel("Efternamn: ");
-		JTextField efternamnInput = new JTextField(20);
+		JTextField efternamnInput = new JTextField(30);
 	    JLabel telLabel = new JLabel("Telefonnummer: ");
-	    JTextField telInput = new JTextField(20);		    
+	    JTextField telInput = new JTextField(30);		    
 	    JLabel mejlLabel = new JLabel("Mejladress: ");
-	    JTextField mejlInput = new JTextField(20);
+	    JTextField mejlInput = new JTextField(30);
 
 	    fn = fornamnInput.getText();		   
 	    en = efternamnInput.getText();
@@ -143,47 +110,142 @@ public class Ui {
 		efternamnInput.setText("");
 	    telInput.setText("");
 	    mejlInput.setText("");
-		    		    
-	    panel.add(fornamnLabel);
-		panel.add(fornamnInput);
-	    panel.add(efternamnLabel);
-	    panel.add(efternamnInput);
-	    panel.add(telLabel);
-	    panel.add(telInput);
-	    panel.add(mejlLabel);
-	    panel.add(mejlInput);
-	    panel.add(sokKnapp);
-
-	    controlPanel.add(panel);
-
 		
-	}
-	
-	private class Knapptryck implements ActionListener {
+	    sokPanel.add(fornamnLabel);
+	    sokPanel.add(fornamnInput);
+	    sokPanel.add(efternamnLabel);
+	    sokPanel.add(efternamnInput);
+	    sokPanel.add(telLabel);
+	    sokPanel.add(telInput);
+	    sokPanel.add(mejlLabel);
+	    sokPanel.add(mejlInput);
+	    sokPanel.add(sokKnapp);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String command = e.getActionCommand();
-//		    JPanel panelo = new JPanel();
-//		    
-//			GridLayout glo = new GridLayout(3,1); // 4 rows, 2 columns
-//			panelo.setLayout(glo);
-			
-			if(command.contentEquals("Lista")) {
-				headerLabel.setText("Här är Kontaktboken");
-//				statusLabel.setText("Lista klickades");
-				visaLista();
-			}else if (command.contentEquals("Sök")) {
-				headerLabel.setText("Sök i Kontaktboken");
-//				statusLabel.setText("Sök klickades");
-				sokning();
-			}else if (command.contentEquals("Lägg till")) {
-				headerLabel.setText("Lägg till kontakt i Kontaktboken");
-//				statusLabel.setText("Lägg till klickades");
-			}else
-				headerLabel.setText("Radera kontakt i Kontaktboken");
-//				statusLabel.setText("Ta bort klickades");
-			
-		}
-	}
+    	return sokPanel;
+    }
+    public JPanel laggTill() {
+    	laggTillPanel = new JPanel();
+    	GridLayout gl = new GridLayout(5,1); 
+    	laggTillPanel.setLayout(gl);
+    	JButton sokKnapp = new JButton("Lägg till");
+	    
+		JLabel fornamnLabel = new JLabel("Förnamn: ");
+		JTextField fornamnInput = new JTextField(30);
+		JLabel efternamnLabel = new JLabel("Efternamn: ");
+		JTextField efternamnInput = new JTextField(30);
+	    JLabel telLabel = new JLabel("Telefonnummer: ");
+	    JTextField telInput = new JTextField(30);		    
+	    JLabel mejlLabel = new JLabel("Mejladress: ");
+	    JTextField mejlInput = new JTextField(30);
+
+	    fn = fornamnInput.getText();		   
+	    en = efternamnInput.getText();
+	    tel = telInput.getText();
+	    mejl = mejlInput.getText();		    
+	    
+	    fornamnInput.setText("");
+		efternamnInput.setText("");
+	    telInput.setText("");
+	    mejlInput.setText("");
+		
+	    laggTillPanel.add(fornamnLabel);
+	    laggTillPanel.add(fornamnInput);
+	    laggTillPanel.add(efternamnLabel);
+	    laggTillPanel.add(efternamnInput);
+	    laggTillPanel.add(telLabel);
+	    laggTillPanel.add(telInput);
+	    laggTillPanel.add(mejlLabel);
+	    laggTillPanel.add(mejlInput);
+	    laggTillPanel.add(sokKnapp);
+
+    	return laggTillPanel;
+    }
+    public JPanel taBort() {
+    	taBortPanel = new JPanel();
+    	GridLayout gl = new GridLayout(5,1); 
+    	taBortPanel.setLayout(gl);
+    	JButton sokKnapp = new JButton("Ta bort");
+	    
+		JLabel fornamnLabel = new JLabel("Förnamn: ");
+		JTextField fornamnInput = new JTextField(30);
+		JLabel efternamnLabel = new JLabel("Efternamn: ");
+		JTextField efternamnInput = new JTextField(30);
+	    JLabel telLabel = new JLabel("Telefonnummer: ");
+	    JTextField telInput = new JTextField(30);		    
+	    JLabel mejlLabel = new JLabel("Mejladress: ");
+	    JTextField mejlInput = new JTextField(30);
+
+	    fn = fornamnInput.getText();		   
+	    en = efternamnInput.getText();
+	    tel = telInput.getText();
+	    mejl = mejlInput.getText();		    
+	    
+	    fornamnInput.setText("");
+		efternamnInput.setText("");
+	    telInput.setText("");
+	    mejlInput.setText("");
+		
+	    taBortPanel.add(fornamnLabel);
+	    taBortPanel.add(fornamnInput);
+	    taBortPanel.add(efternamnLabel);
+	    taBortPanel.add(efternamnInput);
+	    taBortPanel.add(telLabel);
+	    taBortPanel.add(telInput);
+	    taBortPanel.add(mejlLabel);
+	    taBortPanel.add(mejlInput);
+	    taBortPanel.add(sokKnapp);
+
+    	return taBortPanel;
+    }
+
+    public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
+     
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Kontaktbok");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        //Create and set up the content pane.
+        Ui demo = new Ui();
+        demo.addComponentToPane(frame.getContentPane());
+         
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+     
+    public static void skapaUI() {
+        /* Use an appropriate Look and Feel */
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+         
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
 }
