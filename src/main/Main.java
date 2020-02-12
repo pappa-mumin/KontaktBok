b19dd663005c5ec0aef372f5846d2a06e5f086c3
Linn <nyren.linn@gmail.com>
2020-02-12 14:55:26 +0100

    Merge löst

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		Ui ui = new Ui();
-//		ui.skapaUI();
+		Ui ui = new Ui();
+		ui.skapaUI();
 		
 		
 		KontaktBok kb = new KontaktBok();
