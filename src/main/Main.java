22c30cbc0a1c36f9a61fa8df8eeb043784631596
Louise Ahlqvist <louise.k.ahlqvist@gmail.com>
2020-02-12 14:34:59 +0100

    Testat att koppla ihop kontaktarray med att skapa och skriva till fil

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Ui ui = new Ui();
-		ui.skapaUI();
+//		Ui ui = new Ui();
+//		ui.skapaUI();
+		
+		
+		KontaktBok kb = new KontaktBok();
+		Fil fil = new Fil();
+		ByggBok bb = new ByggBok();
+		StringBuilder x = new StringBuilder();
+		
+		kb.läggTill("Louise", "Awesome", "1234545", "louise@awesome.com");
+		kb.läggTill("Linn", "Fabulous", "2345566", "linn@fabulous.se");
+		kb.läggTill("Oscar", "BadAss", "2457757", "oscar@badass.org");
+		
+//		for(Person p : kb.getKontaktLista()) {
+//			System.out.println(p.getFnamn() + " " + p.getEnamn() + " " + p.getMejl() + " " + p.getTel());
+//		}
+		
+		x = bb.skapaText(kb.getKontaktLista());
+		
+		fil.skapaFil(x);
 
 	}
 
